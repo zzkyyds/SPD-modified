7c5d773cf5834db1847bf1c0532726e8b0ca3e18
zzkkk <zhouzikui@czkj1010.com>
2024-07-19 18:11:17 +0800

    思路

---
+++ src/main/java/core/component/LifeComponent.java
@@ -0,0 +1,23 @@
+package core.component;
+
+public class LifeComponent extends AbstractComponent{
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void act() {
+
+    }
+}
